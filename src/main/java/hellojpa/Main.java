805704aced3a78a297fbc713229dd0126f698f4a
taetaetae <taetaetae_@naver.com>
2021-01-12 23:45:38 +0900

    Initial commit

---
+++ src/main/java/hellojpa/Main.java
@@ -0,0 +1,34 @@
+package hellojpa;
+
+import hellojpa.entity.Member;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class Main {
+
+    public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+
+        try {
+            Member member = new Member();
+            member.setId(100L);
+            member.setName("안녕하세요");
+
+            entityManager.persist(member);
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+        } finally {
+            entityManager.close();
+        }
+
+        entityManagerFactory.close();
+    }
+}
